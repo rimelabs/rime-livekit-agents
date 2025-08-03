62a1582b345a1eadb04ff25a90b4577c494e0aae
gokuljs <gokuljs029@gmail.com>
2025-08-03 13:14:12 +0530

    brand fix

--- frontend/app/(app)/layout.tsx
+++ frontend/app/(app)/layout.tsx
@@ -12,29 +12,7 @@ export default async function AppLayout({ children }: AppLayoutProps) {
 
   return (
     <>
-      <header className="fixed top-0 left-0 z-50 hidden w-full flex-row justify-between p-6 md:flex">
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.rime.ai/"
-          className="scale-100 transition-transform duration-300 hover:scale-110"
-        >
-          <Image
-            width={90}
-            height={90}
-            src={logo}
-            alt={`${companyName} Logo`}
-            className="block h-auto max-h-[25px] w-auto dark:hidden"
-          />
-          <Image
-            width={90}
-            height={90}
-            src={logoDark ?? logo}
-            alt={`${companyName} Logo`}
-            className="hidden h-auto max-h-[25px] w-auto dark:block"
-          />
-        </a>
-      </header>
+      {/* */}
       {children}
     </>
   );
